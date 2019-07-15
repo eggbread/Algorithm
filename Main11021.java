944e33e530d9aff23bce153582f2f934efab3cff
eggbread <gyeongmin251@gmail.com>
2019-07-15 11:02:58 +0900

    Num.11021

--- Main11021.java
+++ Main11021.java
@@ -1,10 +1,18 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class Main11021 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for(int i=0;i<n;i++) {
+			int A = scan.nextInt();
+			int B =scan.nextInt();
+			System.out.println("Case #"+(i+1)+": "+(A+B));
+		}
 	}
 
 }
