981f09da2af80067a79f809158e627e8a29a05c2
eggbread <gyeongmin251@gmail.com>
2019-07-15 11:09:37 +0900

    Num.10952

---
+++ Main10952.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Main10952 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNext()) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			System.out.println(A + B);
+		}
+	}
+
+}
