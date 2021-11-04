fbdc1eed6bc54376591cc4913c0795ed0054e518
eggbread <gyeongmin251@gmail.com>
2021-11-05 00:21:55 +0900

    2021_11_04

--- python/level1.py
+++ python/level1.py
@@ -27,5 +27,40 @@ def binary_search(num, win_nums):
         else:
             return binary_search(num, win_nums[:int(len(win_nums) / 2)])
 
+def solution(s):
+    """
+    time : n
+    url : https://programmers.co.kr/learn/courses/30/lessons/81301
+    """
+    dict = {
+        "zero":0,
+        "one":1,
+        "two":2,
+        "three":3,
+        "four":4,
+        "five":5,
+        "six":6,
+        "seven":7,
+        "eight":8,
+        "nine":9
+    }
+    answer = ""
+    i = 0
+    while i < len(s):
+        if s[i].isdigit():
+            answer += s[i]
+            i+=1
+        else:
+            if s[i:i+3] in dict:
+                answer += str(dict[s[i:i+3]])
+                i += 3
+            elif s[i:i+4] in dict:
+                answer += str(dict[s[i:i+4]])
+                i += 4
+            elif s[i:i+5] in dict:
+                answer += str(dict[s[i:i+5]])
+                i += 5
+    return int(answer)
+
 if __name__ == '__main__':
-    pass
+    print(solution("123"))
